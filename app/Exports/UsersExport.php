467ec2de08581cfa86302962e47a5d1a5b66d912
PEDRO HENRIQUE BELCHIOR ALVES <pedro.belchialves2026@gmail.com>
2026-05-14 21:16:41 -0300

    feat: create base excel export structure

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -4,11 +4,52 @@ namespace App\Exports;
 
 use App\Models\User;
 use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class UsersExport implements FromQuery
+class UsersExport implements FromQuery, WithHeadings, WithMapping
 {
     public function query()
     {
-        return User::query();
+        return User::query()
+            ->with([
+                'documento',
+                'endereco',
+                'matriculas.escola',
+            ]);
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Nome',
+            'E-mail',
+            'Data de Nascimento',
+            'Range de Escolaridade',
+            'Escola',
+            'CPF',
+            'RG',
+            'Logradouro',
+            'CEP',
+            'Aprovações',
+            'Reprovações',
+        ];
+    }
+
+    public function map($user): array
+    {
+        return [
+            $user->name,
+            $user->email,
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+        ];
     }
 }
